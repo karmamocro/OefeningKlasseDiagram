097a6e5ebd9df72269394064a593ad7d4368ea83
karmamocro <o.elhajoui@student.fontys.nl>
2018-02-09 11:24:57 +0100

    Add project files.

---
+++ Algoritmiek-Oefening-KlasseDiagram/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmiek_Oefening_KlasseDiagram
+{
+    public class Order
+    {
+        public List<Product> Producten = new List<Product>();
+
+        public double GiveMaximumPrice()
+        {
+            double _max = 0;
+            foreach (Product product in Producten)
+            {
+                _max = (product.Price > _max) ? product.Price : _max; 
+            }
+            return _max;
+        }
+
+        public double GiveAveragePrice()
+        {
+            double _avg = 0;
+            foreach (Product product in Producten)
+            {
+                _avg += product.Price;
+            }
+
+            _avg /= Producten.Count > 0 ? Producten.Count : 1;
+            return _avg;
+        }
+    }
+}
---
+++ Algoritmiek-Oefening-KlasseDiagram/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmiek_Oefening_KlasseDiagram
+{
+    public class Product
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
