b5966822b84afea91ed09378adb14df04d3fa501
karmamocro <o.elhajoui@student.fontys.nl>
2018-02-10 16:07:07 +0100

    Added stopwatches to give me the elapsed time for each method.

--- Algoritmiek-Oefening-KlasseDiagram/Order.cs
+++ Algoritmiek-Oefening-KlasseDiagram/Order.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace AlgoritmiekOefeningKlasseDiagram
 {
@@ -12,16 +13,27 @@ namespace AlgoritmiekOefeningKlasseDiagram
 
         public double GiveMaximumPrice()
         {
+            Stopwatch _sw = new Stopwatch();
+            _sw.Start();
+
             double _max = 0;
             foreach (Product product in Producten)
             {
-                _max = (product.Price > _max) ? product.Price : _max; 
+                _max = (product.Price > _max) ? product.Price : _max;
             }
+
+            _sw.Stop();
+            Console.WriteLine($"Getting max price method Elapsed time: {_sw.Elapsed}");
+            _sw = null;
+
             return _max;
         }
 
         public double GiveAveragePrice()
         {
+            Stopwatch _sw = new Stopwatch();
+            _sw.Start();
+
             double _avg = 0;
             foreach (Product product in Producten)
             {
@@ -29,13 +41,25 @@ namespace AlgoritmiekOefeningKlasseDiagram
             }
 
             _avg /= Producten.Count > 0 ? Producten.Count : 1;
+
+            _sw.Stop();
+            Console.WriteLine($"Getting avarage price method Elapsed time: {_sw.Elapsed}");
+            _sw = null;
+
             return _avg;
         }
 
         public List<Product> GetAllProducts(double minimumPrice)
         {
+            Stopwatch _sw = new Stopwatch();
+            _sw.Start();
             List<Product> _producten = new List<Product>();
             Producten.ForEach((p) => { if (p.Price >= minimumPrice) _producten.Add(p); });
+
+            _sw.Stop();
+            Console.WriteLine($"Get all products from minimum price method Elapsed time: {_sw.Elapsed}");
+            _sw = null;
+
             return _producten;
         }
 
@@ -45,8 +69,11 @@ namespace AlgoritmiekOefeningKlasseDiagram
         /// </summary>
         public void SortProductsByPrice()
         {
-            for (int i = 0; i <= Producten.Count - 1; ++i) {
-                for (int y = 0; y < Producten.Count -1 - i; y++) // ga voor de 2e keer zo skippen we ook de laatste
+            Stopwatch _sw = new Stopwatch();
+            _sw.Start();
+            for (int i = 0; i <= Producten.Count - 1; ++i)
+            {
+                for (int y = 0; y < Producten.Count - 1 - i; y++) // ga voor de 2e keer zo skippen we ook de laatste
                 {
                     if (Producten[y].Price > Producten[y + 1].Price) // controleer de grote
                     {
@@ -57,6 +84,9 @@ namespace AlgoritmiekOefeningKlasseDiagram
                     }
                 }
             }
+            _sw.Stop();
+            Console.WriteLine($"Sorting products Elapsed time: {_sw.Elapsed}");
+            _sw = null;
         }
     }
 }
